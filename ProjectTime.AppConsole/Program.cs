a3d2e792f4bd96997de097fda716f412d5d623a9
loongEgg <loongEgg@users.noreply.github.com>
2020-03-21 22:32:19 +0800

    PT-20-01 创建控制台应用程序，基本的输入输出和字符串分割去空项的方法

--- ProjectTime.AppConsole/Program.cs
+++ ProjectTime.AppConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace ProjectTime.AppConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Project Time~");
+            Console.WriteLine("Please Input Sth:");
+
+            string input = Console.ReadLine();
+            var items = input.Split(new char[] { ' ', ',' }).Where( item => !string.IsNullOrEmpty(item)).ToList();
+            Debugger.Break();
         }
     }
 }
