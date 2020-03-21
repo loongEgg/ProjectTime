23ce8725fd4b6f8f803cf8d514cb1057ed9fd721
loongEgg <loongEgg@users.noreply.github.com>
2020-03-21 22:12:36 +0800

    添加项目文件。

---
+++ ProjectTime.AppConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTime.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
