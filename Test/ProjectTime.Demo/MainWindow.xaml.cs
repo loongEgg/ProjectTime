45f445b7a330ebeb211758363fa6c9c0dc58da81
loongEgg <loongEgg@users.noreply.github.com>
2020-03-22 17:23:53 +0800

    PT-20-03 创建无边框窗体的样式，拖动、最大化、最小化和关闭控制

---
+++ Test/ProjectTime.Demo/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectTime.Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            => this.DragMove();
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            if (e.Source is Button btn)
+            {
+                string cmd = btn.Content.ToString().ToLower();
+                switch (cmd)
+                {
+                    case "minimize": this.WindowState = WindowState.Minimized; break;
+                    case "maximize": this.WindowState ^= WindowState.Maximized; break;
+                    case "close": this.Close(); break;
+                    default:
+                        Debugger.Break();
+                        break;
+                }
+                
+            }
+        }
+    }
+}
