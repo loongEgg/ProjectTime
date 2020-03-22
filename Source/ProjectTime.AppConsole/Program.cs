8afe7dd5226be70c4dbf82c62f5f0f6ade3a0236
loongEgg <loongEgg@users.noreply.github.com>
2020-03-22 13:00:19 +0800

    PT-20-02 初步的项目结构

--- Source/ProjectTime.AppConsole/Program.cs
+++ Source/ProjectTime.AppConsole/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectTime.AppConsole
 {
@@ -11,12 +8,19 @@ namespace ProjectTime.AppConsole
     {
         static void Main(string[] args)
         {
+            // 控制台程序的基本输入输出
             Console.WriteLine("Hello Project Time~");
             Console.WriteLine("Please Input Sth:");
 
             string input = Console.ReadLine();
-            var items = input.Split(new char[] { ' ', ',' }).Where( item => !string.IsNullOrEmpty(item)).ToList();
+
+            // 字符串分割与剔除空项
+            var items = input.Split(new char[] { ' ', ',' })
+                                .Where( 
+                                        item => !string.IsNullOrEmpty(item)
+                              ).ToList();
             Debugger.Break();
+             
         }
     }
 }
